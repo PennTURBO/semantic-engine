856241f9ec3c22d0cca734672a5621701f21ca97
Hayden Freedman <hfree@pmacs.upenn.edu>
2019-03-13 17:37:33 -0400

    stuff

--- drivetrain/src/main/scala/edu/upenn/turbo/ShortcutHomoSapiensFromGraph.scala
+++ drivetrain/src/main/scala/edu/upenn/turbo/ShortcutHomoSapiensFromGraph.scala
@@ -12,52 +12,58 @@ import org.eclipse.rdf4j.model.Value
 
 object ShortcutHomoSapiensFromGraph extends ProjectwideGlobals
 {
-    val typeURI = "http://haydensgraph.org/shortcut_obo_NCBITaxon_9606"
+    //val typeURI = "http://haydensgraph.org/shortcut_obo_NCBITaxon_9606"
+    val typeURI = "http://purl.obolibrary.org/obo/NCBITaxon_9606"
     val pattern = buildPattern(typeURI)  
     
     def buildPattern(typeURI: String): String =
     {
         var pattern = ""
-        val res = getGraphData(typeURI).toSet.toList
+        val res = getGraphData(Array(typeURI))
         for (a <- res)
         {
+            //for (b <- a) print(b)
+            //println()
             var bool = true
             var subject = ""
+            
             if (a(0) == null) subject = a(3).toString
             else subject = a(0).toString
+            
             val predicate = a(1).toString
             var objectVal = ""
+            
             if (a(2) == null) objectVal = a(3).toString
             else objectVal = a(2).toString
+            
             if (a(4) == "false") bool = false
             if (!bool) pattern += "OPTIONAL {\n"
             pattern += "<" + subject + "> <" + predicate + "> <" + objectVal + ">"
             if (!bool) pattern += "}"
             pattern += "\n"
-          println(a(0) + " " + a(1) + " " + a(2) + " " + a(3) + " " + a(4))
+          //println(a(0) + " " + a(1) + " " + a(2) + " " + a(3) + " " + a(4))
         }
         pattern
     }
     
-    def getGraphData(typeURI: String, traversedNodes: HashSet[String] = new HashSet[String]): ArrayBuffer[ArrayBuffer[Value]] =
+    def getGraphData(typeURI: Array[String], traversedNodes: HashSet[String] = new HashSet[String]): ArrayBuffer[ArrayBuffer[Value]] =
     {
-        println("building pattern for uri " + typeURI)
-        println("traversed nodes: ")
-        for (a <- traversedNodes)
+        println()
+        for (a <- traversedNodes) println(a)
+        println()
+        var nextResults = new ArrayBuffer[String]
+        var newSet = traversedNodes
+        var resultsToReturn = new ArrayBuffer[ArrayBuffer[Value]]
+        for (a <- typeURI)
         {
-          println(a)
-        }
-        if (traversedNodes.contains(typeURI)) ArrayBuffer(ArrayBuffer())
-        else
-        {
-            println("did not contain")
+            //println("did not contain")
             val query: String = s"""
             
             Select ?subjValue ?predValue ?objValue ?required ?typeURI Where
             {
-              Values ?typeURI {<$typeURI>}
+              Values ?typeURI {<$a>}
               {
-                ?graphConnection a graph:ObjectConnectionFromShortcuts .
+                ?graphConnection a graph:ConnectionFromShortcutExpansion .
                 ?graphConnection graph:subject ?typeURI .
                 ?graphConnection graph:predicate ?predValue .
                 ?graphConnection graph:object ?objValue .
@@ -65,7 +71,7 @@ object ShortcutHomoSapiensFromGraph extends ProjectwideGlobals
               }
               UNION
               {
-                ?graphConnection a graph:ObjectConnectionFromShortcuts .
+                ?graphConnection a graph:ConnectionFromShortcutExpansion .
                 ?graphConnection graph:subject ?subjValue .
                 ?graphConnection graph:predicate ?predValue .
                 ?graphConnection graph:object ?typeURI .
@@ -74,14 +80,31 @@ object ShortcutHomoSapiensFromGraph extends ProjectwideGlobals
             }
             
             """
-            val newSet = traversedNodes + typeURI
+            newSet = newSet + a
+            
             var patternList = update.querySparqlAndUnpackTuple(sparqlPrefixes + query, Array("subjValue", "predValue", "objValue", "typeURI", "required"))
+            println()
+            println("searched on type: " + a)
             for (a <- patternList)
             {
-                if (a(0) != null) for (b <- getGraphData(a(0).toString, newSet)) if (b.size > 0) patternList += b
-                if (a(2) != null) for (c <- getGraphData(a(2).toString, newSet)) if (c.size > 0) patternList += c
+                var bool = false
+                if (a(0) != null && !traversedNodes.contains(a(0).toString))
+                {
+                    nextResults += a(0).toString
+                    println(a(0))
+                    bool = true
+                }
+                else if (a(2) != null && !traversedNodes.contains(a(2).toString)) 
+                {
+                  nextResults += a(2).toString
+                  println(a(2))
+                  bool = true
+                }
+                println("adding results to return: " + a(0) + " " + a(1) + " " + a(2) + " " + a(3))
+                if (bool) resultsToReturn += a
             }
-            patternList
         }
+        if (nextResults.size > 0) for (a <- getGraphData(nextResults.toArray, newSet)) resultsToReturn += a
+        resultsToReturn
     }
 }
